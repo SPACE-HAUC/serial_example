fdbdf0ecfa0331adb7f2c3709ade3e9b3dea5841
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2020-01-17 15:19:35 -0500

    Implemented data framing

--- serial_client.c
+++ serial_client.c
@@ -8,8 +8,8 @@
 
 #ifdef RPI
 #include <wiringPi.h>
-#define WRITE_SIG 27
-#define READ_SIG 17
+#define WRITE_SIG 2 // 27
+#define READ_SIG 0  // 17
 #endif
 
 int set_interface_attribs(int fd, int speed, int parity)
@@ -45,7 +45,7 @@ int set_interface_attribs(int fd, int speed, int parity)
 
     if (tcsetattr(fd, TCSANOW, &tty) != 0)
     {
-         perror("error from tcsetattr");
+        perror("error from tcsetattr");
         return -1;
     }
     return 0;
@@ -65,7 +65,7 @@ void set_blocking(int fd, int should_block)
     tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
 
     if (tcsetattr(fd, TCSANOW, &tty) != 0)
-         perror("error setting term attributes");
+        perror("error setting term attributes");
 }
 
 #include <stdio.h>
@@ -74,15 +74,15 @@ void set_blocking(int fd, int should_block)
 
 int main(int argc, char **argv)
 {
-    #ifdef RPI
-    if (wiringPiSetup()==-1)
+#ifdef RPI
+    if (wiringPiSetup() == -1)
     {
-        printf("Could not setup signalling using wiringPi. Use sudo..?\n") ;
+        printf("Could not setup signalling using wiringPi. Use sudo..?\n");
         pinMode(READ_SIG, OUTPUT);
         pinMode(WRITE_SIG, OUTPUT);
     }
-    #endif
-    srand(time(NULL)) ;
+#endif
+    srand(time(NULL));
     if (argc != 3)
     {
         printf("Usage: ./serial_client <PORT> <BAUD>");
@@ -95,54 +95,74 @@ int main(int argc, char **argv)
         return 1;
     }
     int baud = atol(argv[2]);
-    unsigned charsleep = 0 ;
+    unsigned charsleep = 0;
     switch (baud)
     {
     case 115200:
         set_interface_attribs(fd, B115200, 0);
-        charsleep = 100 ;
+        charsleep = 100;
         break;
 
     case 9600:
         set_interface_attribs(fd, B9600, 0);
-        charsleep = 1000 ;
+        charsleep = 1000;
         break;
 
     case 230400:
         set_interface_attribs(fd, B230400, 0);
-        charsleep = 50 ;
+        charsleep = 50;
         break;
     default:
         set_interface_attribs(fd, B115200, 0);
-        charsleep = 100 ;
+        charsleep = 100;
         break;
     }
-    set_blocking (fd, 0);
+    set_blocking(fd, 0);
 
-    unsigned char inbuf[34], obuf ;
-    while(1)
+    unsigned char inbuf[34], obuf, tmp;
+    while (1)
     {
-        #ifdef RPI
-        digitalWrite(READ_SIG,1) ;
-        #endif
-        usleep(34*charsleep) ;
-        int n = read (fd, inbuf, 34) ;
-        #ifdef RPI
-        digitalWrite(READ_SIG,0) ;
-        #endif
-        obuf = 0xff & rand() ;
-        #ifdef RPI
-        digitalWrite(WRITE_SIG,1) ;
-        #endif
-        n = write(fd, &obuf , 1) ;
-        #ifdef RPI
-        digitalWrite(WRITE_SIG,0) ;
-        #endif
-        usleep(charsleep) ;
-        printf("Received: ") ;
-        for ( int i = 0 ; i < 34 ; i++ )
-            printf("%d ",  inbuf[i] ) ;
-        printf("\nSent: %d\n", obuf) ;
+#ifdef RPI
+        digitalWrite(READ_SIG, 1);
+#endif
+        // read 10 0xa0 bytes to make sure you got the full frame
+        int preamble_count = -10, frame_valid = 1, wait_count = 0;
+        do
+        {
+            read(fd, &tmp, 1);
+            wait_count++;
+            printf("In waiting: read 0x%x, wait count: %d\n", tmp, wait_count);
+            if (tmp == 0xa0)
+                preamble_count++;
+            else
+                preamble_count = -10;
+        } while ((preamble_count < 0) && (wait_count < 10));
+        // read data
+        int n = read(fd, inbuf, 34); // read actual data
+        // read end frame
+        for (int i = 0; i < 2; i++)
+        {
+            read(fd, &tmp, 1);
+            if (tmp != 0xb0)
+                frame_valid &= 0;
+        }
+#ifdef RPI
+        digitalWrite(READ_SIG, 0);
+#endif
+        obuf = 0xff & rand();
+#ifdef RPI
+        digitalWrite(WRITE_SIG, 1);
+#endif
+        n = write(fd, &obuf, 1);
+#ifdef RPI
+        digitalWrite(WRITE_SIG, 0);
+#endif
+        usleep(charsleep);
+        printf("Frame valid: %d\n", frame_valid);
+        printf("Received: ");
+        for (int i = 0; i < 34; i++)
+            printf("%d ", inbuf[i]);
+        printf("\nSent: %d\n", obuf);
     }
-    return 0 ;
+    return 0;
 }
